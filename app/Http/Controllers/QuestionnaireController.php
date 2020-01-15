2e36e4b750eb3d3246cde31168d460271078b9f8
Salomon Romano <salo_romano@yahoo.com>
2020-01-15 17:25:36 -0600

    First Commit

---
+++ app/Http/Controllers/QuestionnaireController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Questionnaire;
+use Illuminate\Http\Request;
+
+class QuestionnaireController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        return view('questionnaire.create');
+    }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'purpose' => 'required',
+        ]);
+
+        $questionnaire = auth()->user()->questionnaires()->create($data);
+
+        return redirect('/questionnaires/' . $questionnaire->id);
+    }
+
+    public function show(Questionnaire $questionnaire)
+    {
+        $questionnaire->load('questions.answers.responses');
+
+        return view('questionnaire.show', compact('questionnaire'));
+    }
+
+    public function destroy(Questionnaire $questionnaire)
+    {
+        foreach ($questionnaire->questions as $question) {
+            $question->answers()->delete();
+        }
+
+        $questionnaire->questions()->delete();
+
+        $questionnaire->delete();
+
+        return redirect('/home');
+    }
+}
